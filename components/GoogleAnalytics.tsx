94ae06016402c2c72491647c15a272b65581f274
shishir1337 <shishirdevilzz@gmail.com>
2025-01-11 17:40:40 +0600

    Add: Google Analytics

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,12 +2,14 @@
 
 import Script from 'next/script';
 
+const GA_MEASUREMENT_ID = 'G-51QW3V0432';
+
 export const GoogleAnalytics = () => {
   return (
     <>
       <Script
         strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-51QW3V0432"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
       />
       <Script
         id="google-analytics"
@@ -17,7 +19,7 @@ export const GoogleAnalytics = () => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-51QW3V0432');
+            gtag('config', '${GA_MEASUREMENT_ID}');
           `,
         }}
       />
